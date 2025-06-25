67f86919b66a96cd2c05f44c4a31ae92d164fa40
HOUOH9772 <carolli7074@gmail.com>
2025-06-25 14:55:18 -0400

    CremaD_loader.py

--- dataloaders/CremaD_loader.py
+++ dataloaders/CremaD_loader.py
@@ -6,64 +6,70 @@ import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
 from typing import Dict, List, Tuple, Any
 from collections import defaultdict
+from dataset.template import BaseMultimodalDataset, MultimodalSample
 
-class CremaDDataset(Dataset):
+class CremaDDataset(BaseMultimodalDataset):
     EMOTION_LABELS = ['anger', 'disgust', 'fear', 'happy', 'neutral', 'sad']
     EMOTION_MAP = {code: idx for idx, code in enumerate(['ANG', 'DIS', 'FEA', 'HAP', 'NEU', 'SAD'])}
     LEVEL_LABELS = ['low', 'medium', 'high', 'unknown']
     LABEL_MAP = {label: idx for idx, label in enumerate(['LO', 'MD', 'HI', 'XX'])}
 
-    def __init__(self, root_dir):
-        self.root_dir = root_dir
-        self.audio_paths = []
+    def __init__(self, data_dir):
+        super().__init__(modalaity_keys=["audio"])
 
-        self.audio_files = []
-        self.labels = defaultdict(list)
-        for root, _, files in os.walk(self.root_dir):
-            for file in files:
-                if file.endswith(".wav"):
-                    file_name = os.path.splitext(file)[0]
-                    path = os.path.join(root, file)
-                    self.audio_paths.append(path)
-                    label_names = file_name.split('_')
+        self.root_dir = data_dir
+        self.samples = []
 
-                    # Assume that the actor (1) and the sentence spoken (2) aren't important
-                    _, _, emotion, intensity = label_names
-                    label = [-1, -1]
-                    if emotion in self.EMOTION_MAP:
-                        label[0] = self.EMOTION_MAP[emotion]
-                    if intensity in self.LABEL_MAP:
-                        label[1] = self.LABEL_MAP[intensity]
+        for file in os.listdir(data_dir):
+            if file.endswith(".wav"):
+                file_name = os.path.splitext(file)[0]
+                audio_path = os.path.join(data_dir, file)
+                label_names = file_name.split('_')
 
-                    # self.labels should have values in the form of [emotion, intensity]
-                    self.labels[file_name] = label
+                # Assume that the actor (1) and the sentence spoken (2) aren't important
+                actor, sentence, emotion, intensity = label_names
+                if emotion not in self.EMOTION_MAP:
+                    continue
+                emo_label = self.EMOTION_MAP[emotion]
+                if intensity not in self.LABEL_MAP:
+                    continue
+                int_label = self.LABEL_MAP[intensity]
 
-                    self.audio_files.append((path, file_name, label))
+                sample = MultimodalSample(
+                    id=file_name,
+                    audio=audio_path,
+                    label=emo_label,
+                    metadata={
+                        "speaker": actor,
+                        "sentence": sentence,
+                        "intensity": int_label
+                    }
+                )
+                self.samples.append(sample)
     
     def __len__(self):
-        return len(self.audio_files)
+        return len(self.samples)
     
     def __getitem__(self, idx):
-        path, file_name, label = self.audio_files[idx]
+        sample = self.samples[idx]
+        if isinstance(sample.audio, str) and os.path.exists(sample.audio):
+            with open(sample.audio, "rb") as f:
+                sample.audio = f.read()
 
-        with open(path, 'rb') as f:
-            audio_bytes = f.read() # raw audio bytes. Can adjust this line to load audio instead
-
-        return {
-            'audio': audio_bytes,
-            'label': label,
-            'filename': file_name
-        }
+        return sample.to_dict()
 
     @classmethod
-    def get_emotion_and_intensity(cls, label) -> str:
-        ans = ["unknown", "unknown"]
+    def get_emotion(cls, emo_label) -> str:
         for idx, emotion in enumerate(cls.EMOTION_LABELS):
-            if idx == label[0]:
-                ans[0] = emotion
+            if idx == emo_label:
+                return emotion
+        return "unknown"
+    
+    @classmethod
+    def get_intensity(cls, int_label) -> str:
         for idx, intensity in enumerate(cls.LEVEL_LABELS):
-            if idx == label[1]:
-                ans[1] = intensity
-        return ans
+            if idx == int_label:
+                return intensity
+        return "unknown"
 
 
