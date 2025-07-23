dccdf83b2951dde036f2e637577a557dd55f1411
keanepotato <keane.ongweiyang@u.nus.edu>
2025-07-23 22:01:58 +0000

    update human behaviour shell

--- updated_train/examples/_human_behaviour_grpo.sh
+++ updated_train/examples/_human_behaviour_grpo.sh
@@ -9,8 +9,8 @@ python -m verl.trainer.main \
     data.train_files=/home/keaneong/human-behavior/data/instruction/meld_instruction_train.jsonl \
     data.val_files=/home/keaneong/human-behavior/data/instruction/meld_instruction_val.jsonl \
     data.image_dir=/scratch/keane/human_behaviour_data \
-    algorithm.adv_estimator=drpo \
+    algorithm.adv_estimator=grpo \
     worker.actor.model.model_path=Qwen/Qwen2.5-VL-7B-Instruct \
     trainer.n_gpus_per_node=1 \
     worker.actor.optim.lr=5e-6 \
-    trainer.experiment_name=drpo_vanilla_unified
+    trainer.experiment_name=grpo_vanilla_unified
