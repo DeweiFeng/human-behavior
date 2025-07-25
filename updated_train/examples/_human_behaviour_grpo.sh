ae28edbcc17b06055ab32543b5a5b21eae7014bb
keanepotato <keane.ongweiyang@u.nus.edu>
2025-07-25 22:17:28 +0000

    sync shell

--- updated_train/examples/_human_behaviour_grpo.sh
+++ updated_train/examples/_human_behaviour_grpo.sh
@@ -4,7 +4,12 @@ set -euo pipefail
 # Optional: activate your virtual environment here if needed
 # source /path/to/venv/bin/activate
 
-python -m verl.trainer.main \
+# Train files is what you are training on
+# Val files is what you are validating on
+# Image directory is where the images are stored
+# This does not cover testing
+
+python -m /home/human-behavior/updated_train/verl/trainer/main.py \
     config=/home/keaneong/human-behavior/updated_train/examples/grpo_climb_engaging.yaml \
     data.train_files=/home/keaneong/human-behavior/data/instruction/meld_instruction_train.jsonl \
     data.val_files=/home/keaneong/human-behavior/data/instruction/meld_instruction_val.jsonl \
